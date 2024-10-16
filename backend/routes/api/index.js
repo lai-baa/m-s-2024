bf5074b36f329b7fa43c685ba91f6b51fdc706d7
Laiba Afzal <laiba.a1298@gmail.com>
2024-10-15 23:29:22 -0400

    added API router

---
+++ backend/routes/api/index.js
@@ -0,0 +1,12 @@
+// backend/routes/api/index.js
+const router = require('express').Router();
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
+
+module.exports = router;
