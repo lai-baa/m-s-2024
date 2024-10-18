57491d5e5a354058b16f98f01eec83257fa6cc26
Laiba Afzal <laiba.a1298@gmail.com>
2024-10-17 22:33:12 -0400

    added admin login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,16 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const adminRouter = require("./admin.js");
 const { restoreAdmin } = require('../../utils/auth.js');
 
 router.use(restoreAdmin);
+
+router.use('/session', sessionRouter);
+router.use('/admin', adminRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});  
 
 module.exports = router;
---
+++ backend/routes/api/session.js
@@ -0,0 +1,48 @@
+// backend/routes/api/session.js
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, restoreAdmin } = require('../../utils/auth');
+const { Admin } = require('../../db/models');
+
+const router = express.Router();
+
+// Log in
+router.post(
+    '/',
+    async (req, res, next) => {
+      const { credential, password } = req.body;
+  
+      const admin = await Admin.unscoped().findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+  
+      if (!admin || !bcrypt.compareSync(password, admin.hashedPassword.toString())) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = { credential: 'The provided credentials were invalid.' };
+        return next(err);
+      }
+  
+      const safeAdmin = {
+        id: admin.id,
+        email: admin.email,
+        username: admin.username,
+      };
+  
+      await setTokenCookie(res, safeAdmin);
+  
+      return res.json({
+        admin: safeAdmin
+      });
+    }
+);
+
+module.exports = router;
