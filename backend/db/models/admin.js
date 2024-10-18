46426431c4e97fafc67d24c50619a352b19db596
Laiba Afzal <laiba.a1298@gmail.com>
2024-10-17 22:18:52 -0400

    added admins table migration and seeds

---
+++ backend/db/models/admin.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const { Model, Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Admin extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Admin.init({
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error('Cannot be an email.');
+          }
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true,
+      },
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60],
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Admin',
+  });
+  return Admin;
+};
