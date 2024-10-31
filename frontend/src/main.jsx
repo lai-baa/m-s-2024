692e0f538bfaeaa90b0bdfcadeabe61132db30fc
Laiba Afzal <laiba.a1298@gmail.com>
2024-10-30 22:30:56 -0400

    setting up admin access in actions and reducers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
