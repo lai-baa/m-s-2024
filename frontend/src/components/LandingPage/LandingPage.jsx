40c13204922b2c96f91ad0ea275bb9ac6e004556
Laiba Afzal <laiba.a1298@gmail.com>
2024-12-11 22:58:40 -0500

    creating landing page

---
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+// import { useState, useEffect, useRef } from 'react';
+// import { useDispatch } from 'react-redux';
+import Navigation from "../Navigation/Navigation";
+
+function LandingPage() {
+    
+    return (
+        <Navigation />
+    )
+}
+
+export default LandingPage;
