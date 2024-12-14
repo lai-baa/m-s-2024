d8d49a86309b511371097b13e74b0d697c87250e
Laiba Afzal <laiba.a1298@gmail.com>
2024-12-13 23:44:51 -0500

    rsvp modal

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 // import { useState, useEffect, useRef } from 'react';
 // import { useDispatch } from 'react-redux';
 import Navigation from "../Navigation/Navigation";
+import RSVPModal from "../RSVPModal/RSVPModal";
 
 function LandingPage() {
     
@@ -8,7 +9,7 @@ function LandingPage() {
         <>
             <Navigation />
             <p>RSVP for Mariam and Shakar&apos;s wedding!</p>
-            
+            <RSVPModal />
         </>
         
     )
---
+++ frontend/src/components/RSVPModal/RSVPModal.jsx
@@ -0,0 +1,15 @@
+
+
+function RSVPModal() {
+    return (
+        <>
+            <p>Name:</p>
+            <p>Email:</p>
+            <p>Phone Number:</p>
+            <p>Number of attendees:</p>
+            <button>Submit</button>
+        </>
+    )
+}
+
+export default RSVPModal;
