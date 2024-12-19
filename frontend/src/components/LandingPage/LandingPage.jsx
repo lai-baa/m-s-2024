ced312f55221d05c8e0684b197432e29fc0201d9
Laiba Afzal <laiba.a1298@gmail.com>
2024-12-18 22:30:07 -0500

    linking rsvp modal to rsvp text on landing page

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,18 +1,22 @@
-// import { useState, useEffect, useRef } from 'react';
-// import { useDispatch } from 'react-redux';
-import Navigation from "../Navigation/Navigation";
-import RSVPModal from "../RSVPModal/RSVPModal";
+import { useModal } from '../../context/Modal';
+import Navigation from '../Navigation/Navigation';
+import RSVPModal from '../RSVPModal/RSVPModal';
 
 function LandingPage() {
-    
+    const { setModalContent } = useModal();
+
+    const openRSVPModal = () => {
+        setModalContent(<RSVPModal />);
+    };
+
     return (
         <>
             <Navigation />
-            <p>RSVP for Mariam and Shakar&apos;s wedding!</p>
-            <RSVPModal />
+            <p onClick={openRSVPModal} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
+                RSVP for Mariam and Shakar&apos;s wedding!
+            </p>
         </>
-        
-    )
+    );
 }
 
 export default LandingPage;
