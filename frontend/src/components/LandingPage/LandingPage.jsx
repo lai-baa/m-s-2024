aa7b45a2b714e707c52d944bf4fdeeb71f19f09b
Laiba Afzal <laiba.a1298@gmail.com>
2024-12-19 23:33:01 -0500

    adding to landing page

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -12,9 +12,43 @@ function LandingPage() {
     return (
         <>
             <Navigation />
-            <p onClick={openRSVPModal} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
+            <h1>MARIAM & SHAKAR TIE THE KNOT</h1>
+            <h2>Join us for the celebration of love</h2>
+            <div id='fun-facts-div'>
+                <h2>Fun Facts About the Couple</h2>
+                <p>Both Mariam and Shakar work in law enforcement. Mariam is a lawyer and Shakar is a police officer.</p>
+                <p>Both Mariam and Shakar have one dimple on the same side.</p>
+            </div>
+            <div>
+                <img />
+                <p>
+                    We are incredibly grateful to have you here with us on this momentous day.
+                    Your presence fills our heart with warmth an joy, and we cannot thank you 
+                    enough for sharing in our joyous occasion. 
+
+                    Your love and support have been unwavering, and we are deeply touched by 
+                    the sacrifices you have made to be here. Our hearts are overflowing with
+                    gratitude for the role you have played in our lives, and we feel blessed 
+                    to have such amazing family and friends.
+
+                    - Shakar and Mariam
+                </p>
+            </div>
+            <div id='quran-ver-div'>
+                <h2 id='quran-ver-heading'>IT IS NARRATED (30:21)</h2>
+                <p id='quran-verse'>
+                    And one of His signs is that He created you spouses from 
+                    among yourselves so that you may find comfort in them. 
+                    And He has placed between you compassion and mercy. 
+                    Surely in this are signs for people who reflect.
+                </p>
+                <h3 id='quran-verse-surah'>Surah Ar-Rum 21</h3>
+            </div>
+            <h3 onClick={openRSVPModal} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
                 RSVP for Mariam and Shakar&apos;s wedding!
-            </p>
+            </h3>
+
+
         </>
     );
 }
