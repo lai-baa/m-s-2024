9525b1ba2c1bc451413cca126d4574b670284384
Laiba Afzal <laiba.a1298@gmail.com>
2025-02-25 23:09:09 -0500

    setting up server for rsvps in backend

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,12 +2,14 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const adminRouter = require("./admin.js");
+const rsvpRoutes = require("./rsvps.js");
 const { restoreAdmin } = require('../../utils/auth.js');
 
 router.use(restoreAdmin);
 
 router.use('/session', sessionRouter);
 router.use('/admin', adminRouter);
+router.use("/api/rsvps", rsvpRoutes);
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
