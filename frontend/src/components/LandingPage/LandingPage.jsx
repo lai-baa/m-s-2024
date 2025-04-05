1823ea542eba4e7f006eba39e04d635373a1f993
Laiba Afzal <laiba.a1298@gmail.com>
2025-04-04 23:29:04 -0400

    adding venue details

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -31,7 +31,9 @@ function LandingPage() {
             <h1>MARIAM & SHAKAR TIE THE KNOT</h1>
             <h2>Join us for the celebration of love</h2>
             <h2>Save the date: September 10th, 2025</h2>
-            <h2>Venue: ***placeholder***</h2>
+            <h2>Venue: Suleymaniye Mosque</h2>
+            <h2>459 Deer Park Rd</h2>
+            <h2>Dix Hills, NY 11746</h2>
             <div id="quran-ver-div" className={styles.quranVerse}>
                 <h2 id="quran-ver-heading">IT IS NARRATED (30:21)</h2>
                 <p id="quran-verse">
