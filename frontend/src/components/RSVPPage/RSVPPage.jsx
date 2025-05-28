6d591b4a28173f3fdec19d98703e618ad2285d99
Laiba Afzal <laiba.a1298@gmail.com>
2025-05-27 21:14:23 -0400

    rsvp page styling

--- frontend/src/components/RSVPPage/RSVPPage.jsx
+++ frontend/src/components/RSVPPage/RSVPPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './RSVPPage.css';
 
 function RSVPPage() {
   const [rsvps, setRsvps] = useState([]);
@@ -34,26 +35,28 @@ function RSVPPage() {
       {rsvps.length === 0 ? (
         <p>No RSVPs yet.</p>
       ) : (
-        <table border="1" cellPadding="10">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Attendees</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rsvps.map((rsvp) => (
-              <tr key={rsvp.id}>
-                <td>{rsvp.name}</td>
-                <td>{rsvp.email}</td>
-                <td>{rsvp.phone}</td>
-                <td>{rsvp.attendees}</td>
+        <div className="centered-table">
+          <table border="1" cellPadding="10">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Attendees</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {rsvps.map((rsvp) => (
+                <tr key={rsvp.id}>
+                  <td>{rsvp.name}</td>
+                  <td>{rsvp.email}</td>
+                  <td>{rsvp.phone}</td>
+                  <td>{rsvp.attendees}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
